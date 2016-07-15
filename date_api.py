76b39d5e0e54e6ba7866f55e44b2bd6aff8ec822
Amon Gergely Farkas <farkasamongergely@gmail.com>
2016-07-15 20:47:03 +0200

    cs

--- date_api.py
+++ date_api.py
@@ -1,6 +1,20 @@
-import datetime
+def date():
+    import datetime
+    data = {}
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+              "November",
+              "December"]
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
-          "December"]
-days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    date = datetime.datetime.now()
+    weekday = date.weekday()
 
+    data["month"] = months[date.month]
+    data["day"] = str(date.day)
+    data["weekday"] = days[weekday]
+
+    return data
+
+
+if __name__ == "__main__":
+   print date()["month"]+" "+ date()["day"] + "," + date()["weekday"]
