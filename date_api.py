7a1875754c4d4b7bef3c96fe6686f03e837c2f13
Amon Gergely Farkas <farkasamongergely@gmail.com>
2016-07-15 20:15:45 +0200

    Added the date api

--- date_api.py
+++ date_api.py
@@ -3,6 +3,4 @@ import datetime
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
           "December"]
 days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-date = datetime.datetime.now()
-print months[date.month - 1] +" "+ str(date.day)
-print days[date.weekday()]
+
